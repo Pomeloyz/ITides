42c30040d3098ecafcc9647fb317ff048c3602a9
Pomelo <pomeloyz@foxmail.com>
2019-07-27 14:17:48 +0800

    :tada: Initial commit

---
+++ app/src/main/java/oms/pomelo/itides/base/ITideApplication.java
@@ -0,0 +1,14 @@
+package oms.pomelo.itides.base;
+
+import android.app.Application;
+
+/**
+ * NAME: Sherry
+ * DATE: 2019-07-27
+ */
+public class ITideApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/oms/pomelo/itides/ui/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package oms.pomelo.itides.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import oms.pomelo.itides.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
