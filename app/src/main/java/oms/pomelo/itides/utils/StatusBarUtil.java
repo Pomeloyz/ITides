e8a3a4dd603db8ce8d4a0cbade7f4ef7eafbbd1e
XF-zhjnc <zhjncxf@outlook.com>
2019-07-31 21:53:54 +0800

    :sparkles: Add StatusBarUtil class

---
+++ app/src/main/java/oms/pomelo/itides/utils/StatusBarUtil.java
@@ -0,0 +1,170 @@
+package oms.pomelo.itides.utils;
+
+import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+
+import androidx.annotation.ColorInt;
+
+/**
+ * NAME: Lay
+ * DATE: 2019-07-30
+ */
+public class StatusBarUtil {
+
+    /**
+     * 获取状态栏高度
+     *
+     * @param context context
+     * @return 状态栏高度
+     */
+    public static int getStatusBarHeight(Context context) {
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        return context.getResources().getDimensionPixelSize(resourceId);
+    }
+
+    /**
+     * 设置状态栏颜色
+     *
+     * @param activity       需要设置的Activity
+     * @param color          状态栏颜色值
+     * @param statusBarAlpha 状态栏透明度
+     */
+    public static void setColor(Activity activity, @ColorInt int color, int statusBarAlpha) {
+        activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        activity.getWindow().setStatusBarColor(calculateStatusColor(color, statusBarAlpha));
+    }
+
+    /**
+     * 设置状态栏纯色 不加半透明效果
+     *
+     * @param activity 需要设置的Activity
+     * @param color    状态栏颜色值
+     */
+    public static void setColorNoTranslucent(Activity activity, @ColorInt int color) {
+        setColor(activity, color, 0);
+    }
+
+    /**
+     * 设置状态栏全透明
+     *
+     * @param activity 需要设置的Activity
+     */
+    public static void setTransparent(Activity activity) {
+        transparentStatusBar(activity);
+        setRootView(activity);
+    }
+
+    /**
+     * 为头部是 ImageView 的界面设置状态栏全透明
+     *
+     * @param activity       需要设置的Activity
+     * @param needOffsetView 需要向下偏移的View
+     */
+    public static void setTransparentForImageView(Activity activity, View needOffsetView) {
+        setTranslucentForImageView(activity, 0, needOffsetView);
+    }
+
+    /**
+     * 为头部是 ImageView 的界面设置状态栏透明
+     *
+     * @param activity       需要设置的Activity
+     * @param statusBarAlpha 状态栏透明度
+     * @param needOffsetView 需要向下偏移的View
+     */
+    public static void setTranslucentForImageView(Activity activity, int statusBarAlpha, View needOffsetView) {
+        activity.getWindow().setStatusBarColor(Color.TRANSPARENT);
+        activity.getWindow()
+                .getDecorView()
+                .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        if (activity instanceof TabActivity) {
+            activity.getWindow()//兼容TabActivity
+                    .setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+
+        addTranslucentView(activity, statusBarAlpha);
+        if (needOffsetView != null) {
+            ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) needOffsetView.getLayoutParams();
+            if (layoutParams != null) {
+                layoutParams.setMargins(0, getStatusBarHeight(activity), 0, 0);
+            }
+        }
+    }
+
+    //=================================以下是私有方法=================================
+
+    /**
+     * 添加半透明矩形条
+     *
+     * @param activity       需要设置的Activity
+     * @param statusBarAlpha 透明值
+     */
+    private static void addTranslucentView(Activity activity, int statusBarAlpha) {
+        ViewGroup contentView = activity.findViewById(android.R.id.content);
+        if (contentView.getChildCount() > 1) {
+            contentView.getChildAt(1).setBackgroundColor(Color.argb(statusBarAlpha, 0, 0, 0));
+        } else {
+            contentView.addView(createTranslucentStatusBarView(activity, statusBarAlpha));
+        }
+    }
+
+    /**
+     * 设置根布局参数
+     */
+    private static void setRootView(Activity activity) {
+        ViewGroup rootView = (ViewGroup) ((ViewGroup) activity.findViewById(android.R.id.content)).getChildAt(0);
+        rootView.setFitsSystemWindows(true);
+        rootView.setClipToPadding(true);
+    }
+
+    /**
+     * 使状态栏透明
+     */
+    private static void transparentStatusBar(Activity activity) {
+        activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        activity.getWindow().setStatusBarColor(Color.TRANSPARENT);
+    }
+
+    /**
+     * 创建半透明矩形 View
+     *
+     * @param alpha 透明值
+     * @return 半透明View
+     */
+    private static StatusBarView createTranslucentStatusBarView(Activity activity, int alpha) {
+        // 绘制一个和状态栏一样高的矩形
+        StatusBarView statusBarView = new StatusBarView(activity);
+        LinearLayout.LayoutParams params =
+                new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, getStatusBarHeight(activity));
+        statusBarView.setLayoutParams(params);
+        statusBarView.setBackgroundColor(Color.argb(alpha, 0, 0, 0));
+        return statusBarView;
+    }
+
+    /**
+     * 计算状态栏颜色
+     *
+     * @param color color值
+     * @param alpha alpha值
+     * @return 最终的状态栏颜色
+     */
+    private static int calculateStatusColor(@ColorInt int color, int alpha) {
+        float a = 1 - alpha / 255f;
+        int red = color >> 16 & 0xff;
+        int green = color >> 8 & 0xff;
+        int blue = color & 0xff;
+        red = (int) (red * a + 0.5);
+        green = (int) (green * a + 0.5);
+        blue = (int) (blue * a + 0.5);
+        return 0xff << 24 | red << 16 | green << 8 | blue;
+    }
+
+}
---
+++ app/src/main/java/oms/pomelo/itides/utils/StatusBarView.java
@@ -0,0 +1,27 @@
+package oms.pomelo.itides.utils;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+/**
+ * NAME: Lay
+ * DATE: 2019-07-30
+ */
+public class StatusBarView extends View {
+
+    public StatusBarView(Context context) {
+        super(context);
+    }
+
+    public StatusBarView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public StatusBarView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
